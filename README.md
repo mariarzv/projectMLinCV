5ff2bfa0315c2db6cf1dacd30e68525dc3dc314e
mariarzv <maria.razzv@gmail.com>
2023-02-13 19:03:57 +0800

    setup

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # projectMLinCV
 Project "Machine learning in computer vision"
+
+# Demo projects
+
+## YOLOv5 with TensorFlow Lite in C++
+https://github.com/iwatake2222/play_with_tflite/tree/master/pj_tflite_det_yolov5
+
+
+Tested on Windows 10, in PyCharm
---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='projectMLinCV',
+    version='0.0.1',
+    packages=[''],
+    url='https://github.com/mariarzv/projectMLinCV',
+    license='GNU',
+    author='mariarv',
+    author_email='maria.razzv@gmail.com',
+    description='yolov5'
+)
