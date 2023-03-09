37a244ce407f67a183e57a4328033cbb25e13b8a
mariarzv <maria.razzv@gmail.com>
2023-03-09 16:54:40 +0800

    streamlit image load and yolo

--- 22_yolov5s_new/streamlitapp.py
+++ 22_yolov5s_new/streamlitapp.py
@@ -2,16 +2,57 @@ import streamlit as st
 from utils import getprojdir
 import pandas as pd
 import numpy as np
-import os, urllib, cv2
+import os
+import urllib
+import cv2
+from PIL import Image
+from yolov5_tflite_image_inference import detect_image
 
 readme = os.path.normpath(getprojdir() + '/readme.md')
+weights = os.path.normpath(getprojdir() + '/tests/yolov5s-fp16.tflite')
+tempimg = os.path.normpath(getprojdir() + '/temp/temp.jpg')
+detectedimg = os.path.normpath(getprojdir() + '/temp/tempyolov5_output.jpg')
 
-# Streamlit encourages well-structured code, like starting execution in a main() function.
+def detect_userimage(usrimage):
+    detect_image(
+        weights,
+        usrimage,
+        416,
+        0.25,
+        0.45)
+
+# streamlit encourages well-structured code, like starting execution in a main() function.
+
+
 def main():
+    # get readme file content
     with open(readme, "r") as file:
         rmcontent = file.read()
-    # Render the readme as markdown using st.markdown.
-    readme_text = st.markdown(rmcontent)
+
+    st.set_page_config(layout="wide")
+
+    # create two columns with a 1:1 ratio
+    left_column, right_column = st.columns((1, 1))
+
+    with left_column:
+        # render the readme as markdown using st.markdown.
+        readme_text = st.markdown(rmcontent)
+
+    with right_column:
+        st.title("Load your .jpg image for processing:")
+
+        uploaded_file = st.file_uploader("Choose an image...", type="jpg")
+
+        if uploaded_file is not None:
+            image = Image.open(uploaded_file)
+            image.save(tempimg)
+            st.image(image, caption="Uploaded Image")
+
+        if st.button("Image inference"):
+            # Call the method when the button is clicked
+            detect_userimage(tempimg)
+            imagedet = Image.open(detectedimg)
+            st.image(imagedet, caption="Yolov5 result")
 
 
 if __name__ == "__main__":
